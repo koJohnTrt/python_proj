86f5daa81f2ce52bdea3947313d6e39917311011
thurein <93116497+koJohnTrt@users.noreply.github.com>
2024-03-31 00:40:38 +0630

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, Request, Form, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.middleware.sessions import SessionMiddleware
 
 from db import SessionLocal
 from utils.db_utils import get_db
@@ -20,7 +23,11 @@ async def login_form(request: Request):
 async def login(request: Request, username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
     user = db.query(User).filter(User.username == username, User.password == password).first()
     if user:
-        return templates.TemplateResponse("success.html", {"request": request, "username": username})
+        # Store user information in session
+        request.session['user_id'] = user.id
+        request.session['username'] = user.username
+
+        return templates.TemplateResponse("success.html", {"request": request, "username": request.session.get("username")})
     else:
         return templates.TemplateResponse("failure.html", {"request": request})
 
@@ -32,10 +39,15 @@ async def register_form(request: Request):
 
 @router.post("/register", response_class=HTMLResponse)
 async def register(request: Request, username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.username == username).first()
+    if existing_user:
+        return templates.TemplateResponse("failure.html", {"request": request})
+
     user = User(username=username, password=password)
     db.add(user)
     db.commit()
     if user:
-        return templates.TemplateResponse("success.html", {"request": request, "username": username})
+        request.session['username'] = user.username
+        return templates.TemplateResponse("success.html", {"request": request, "username": request.session.get("username")})
     else:
         return templates.TemplateResponse("failure.html", {"request": request})
