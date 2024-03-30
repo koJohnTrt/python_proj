c8e721828cdeb78a22cdab6b134bd465053e5f82
thurein <93116497+koJohnTrt@users.noreply.github.com>
2024-03-31 00:40:55 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from sqlalchemy.orm import Session, sessionmaker
 from starlette.requests import Request
+from starlette.middleware.sessions import SessionMiddleware
+
 from routers import auth
 from db import engine, session
 
@@ -12,10 +14,12 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 app.include_router(auth.router)
 
+# Add session middleware
+app.add_middleware(SessionMiddleware, secret_key="mysecretkey", session_cookie="login_session")
+
 @app.middleware("http")
 async def db_session_middleware(request: Request, call_next):
     request.state.db = session
     response = await call_next(request)
     request.state.db.close()
     return response
-
